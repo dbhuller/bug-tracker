675074da3def39a0b7705c9984c3f562e6229b90
dbhuller <dbhuller@mail.sfsu.edu>
2021-02-22 20:34:00 -0800

    added login component and basic UI/styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
-import "./App.css";
+import React from "react";
+import Login from "./Views/Login/Login";
+import { useSelector } from "react-redux";
 
 function App() {
-  return <></>;
+  const { auth } = useSelector((state) => state);
+  return <>{!auth.loggedIn ? <Login /> : <></>}</>;
 }
 
 export default App;
